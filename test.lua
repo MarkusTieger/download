feeb3817e7566bfb0b1a04b2576bf5e0adeb571b
MarkusTieger <77546847+MarkusTieger@users.noreply.github.com>
2023-06-16 15:14:01 +0200

    Update test.lua

--- test.lua
+++ test.lua
@@ -2,7 +2,9 @@
 chatSendEventClass = java.resolveClass("de.markustieger.tac.v5.event.impl.ChatMessageSendedEvent")
 
 function onChatMessageSended(message)
-  
+  if message == "pause" then
+    gui.openScreen(java.lua_newInstance("net.minecraft.client.gui.screen.GameMenuScreen", true))
+  end
 end
 
 event.registerListener(chatSendEventClass, 0, java.functionToConsumer(onChatMessageSended))
